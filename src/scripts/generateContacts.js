f5931652dbd8c06088db048d6fd3cf396982840f
Iryna <ikalchenko23@gmail.com>
2025-06-07 15:46:31 +0300

    homework node 1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import {createFakeContact} from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+
+const generateContacts = async (number) => {
+
+    const contactList = await readContacts();
+    for (let i=0; i<number; i++) {
+const newContact = createFakeContact();
+contactList.push(newContact);
+
+    };
+    await writeContacts(contactList);
+};
 
 generateContacts(5);
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    try {
+        const contactListBuffer = await fs.readFile(PATH_DB);
+        return JSON.parse(contactListBuffer);
+    } catch (error) {
+        console.error('Помилка:', error.message);
+        return [];
+    }
+
+};
